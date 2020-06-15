bc9cb82b24ee4a66a8c70a98851870350d8ac8f7
Emmanuel Cruz <em4n0101@gmail.com>
2020-06-15 14:04:42 -0500

    (Animation) Support flip card animation, changes the side from the switch

--- Week3/app/src/main/java/com/em4n0101/myapplication/MainActivity.kt
+++ Week3/app/src/main/java/com/em4n0101/myapplication/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.em4n0101.myapplication
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 import com.em4n0101.myapplication.utilities.Utilities
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Properties
+     */
+    private lateinit var frontAnim: AnimatorSet
+    private lateinit var backAnim: AnimatorSet
+    private var isFront: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         populatePersonUI()
+
+        val scale = applicationContext.resources.displayMetrics.density
+        val cameraDistanceScaleMultiply = 8000
+        cardView.cameraDistance = cameraDistanceScaleMultiply * scale
+        cardViewBack.cameraDistance = cameraDistanceScaleMultiply * scale
+
+        frontAnim = AnimatorInflater.loadAnimator(this, R.animator.front_animator) as AnimatorSet
+        backAnim = AnimatorInflater.loadAnimator(this, R.animator.back_animator) as AnimatorSet
+
+        switch_flip_card.setOnClickListener {
+            val tempSwitch = it as? Switch
+            if (tempSwitch != null) {
+                isFront = if (it.isChecked) {
+                    frontAnim.setTarget(cardView)
+                    backAnim.setTarget(cardViewBack)
+                    frontAnim.start()
+                    backAnim.start()
+                    it.text = getString(R.string.cardBack)
+                    false
+                } else {
+                    frontAnim.setTarget(cardViewBack)
+                    backAnim.setTarget(cardView)
+                    backAnim.start()
+                    frontAnim.start()
+                    it.text = getString(R.string.cardFront)
+                    true
+                }
+            } else {
+                return@setOnClickListener
+            }
+        }
+
     }
 
     /**
