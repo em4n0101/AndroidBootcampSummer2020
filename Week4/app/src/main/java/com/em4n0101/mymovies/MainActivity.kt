53ca35c32eaf400aebff8c9fa55c475fdb463b79
Emmanuel Cruz <em4n0101@gmail.com>
2020-06-22 12:21:45 -0500

    (Code) Integrate the adapter with the recycler view

--- Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
+++ Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
@@ -2,10 +2,20 @@ package com.em4n0101.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.em4n0101.mymovies.utils.Utilities
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get model
+        val movieList = Utilities.createMovies(this)
+
+        // setup recycler
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = MoviesAdapter(movieList)
     }
 }
