67401b8e6ee9b42d3f5c7130e2401a3c329f7158
Emmanuel Cruz <em4n0101@gmail.com>
2020-06-23 00:55:00 -0500

    (Refactoring) Integrate Navigation Component

--- Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
+++ Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
@@ -2,18 +2,15 @@ package com.em4n0101.mymovies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 
 
 class MainActivity : AppCompatActivity() {
-
-    private var moviesFragment = MoviesFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, moviesFragment)
-            .commit()
+        Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 }
