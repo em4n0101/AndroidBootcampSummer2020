41fdf9b1b5aa4ce8b0e898371af9324e92d3148b
Emmanuel Cruz <em4n0101@gmail.com>
2020-06-23 00:44:07 -0500

    (Refactoring) Convert main activity to fragment

--- Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
+++ Week4/app/src/main/java/com/em4n0101/mymovies/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.em4n0101.mymovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import com.em4n0101.mymovies.utils.Utilities
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var moviesFragment = MoviesFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get model
-        val movieList = Utilities.createMovies(this)
-
-        // setup recycler
-        recycler_view.layoutManager = GridLayoutManager(this, 2)
-        recycler_view.adapter = MoviesAdapter(movieList)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, moviesFragment)
+            .commit()
     }
 }
---
+++ Week4/app/src/main/java/com/em4n0101/mymovies/MoviesFragment.kt
@@ -0,0 +1,44 @@
+package com.em4n0101.mymovies
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.em4n0101.mymovies.utils.Utilities
+import kotlinx.android.synthetic.main.fragment_movies.*
+
+class MoviesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.let {
+            // get model
+            val movieList = Utilities.createMovies(it)
+
+            // setup recycler
+            recycler_view.layoutManager = GridLayoutManager(it, 2)
+            recycler_view.adapter = MoviesAdapter(movieList)
+        }
+    }
+
+    companion object {
+        fun newInstance(): MoviesFragment {
+            return MoviesFragment()
+        }
+    }
+}
