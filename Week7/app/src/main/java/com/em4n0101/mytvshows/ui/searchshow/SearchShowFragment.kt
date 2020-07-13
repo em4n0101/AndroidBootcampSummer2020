a2785e3012fa6e7b6467561bc283077028d965fb
Emmanuel Cruz <em4n0101@gmail.com>
2020-07-13 13:14:48 -0500

    (Code) Add menu item for search

--- Week7/app/src/main/java/com/em4n0101/mytvshows/ui/searchshow/SearchShowFragment.kt
+++ Week7/app/src/main/java/com/em4n0101/mytvshows/ui/searchshow/SearchShowFragment.kt
@@ -1,16 +1,18 @@
 package com.em4n0101.mytvshows.ui.searchshow
 
 import android.os.Bundle
+import android.view.*
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.em4n0101.mytvshows.R
 
 class SearchShowFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -19,6 +21,30 @@ class SearchShowFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_show, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.actionSearchShow)
+        if (searchItem != null) {
+            val searchView = searchItem.actionView as SearchView
+            val editText = searchView.findViewById<EditText>(R.id.search_src_text)
+            editText.hint = getString(R.string.hintSearchShow)
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    newText?.let {
+                        Toast.makeText(activity!!, newText, Toast.LENGTH_SHORT).show()
+                    }
+                    return true
+                }
+            })
+        }
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     companion object {
