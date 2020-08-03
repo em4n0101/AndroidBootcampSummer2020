54fa410477c672430012093707193f91cd14ca52
Emmanuel Cruz <em4n0101@gmail.com>
2020-08-03 17:28:26 -0500

    (Code) Refactoring database

--- Week10/app/src/main/java/com/em4n0101/mytvshows/model/database/ShowsDatabase.kt
+++ Week10/app/src/main/java/com/em4n0101/mytvshows/model/database/ShowsDatabase.kt
@@ -1,6 +1,8 @@
 package com.em4n0101.mytvshows.model.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.em4n0101.mytvshows.model.Person
@@ -11,4 +13,32 @@ import com.em4n0101.mytvshows.model.response.ScheduleResponse
 @TypeConverters(ShowConverters::class, PersonConverters::class, ScheduleResponseConverters::class)
 abstract class ShowsDatabase: RoomDatabase() {
     abstract val showsDatabaseDao: ShowsDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ShowsDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): ShowsDatabase {
+            val dataBaseName = "shows_database"
+
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ShowsDatabase::class.java,
+                    dataBaseName
+                )
+                    .fallbackToDestructiveMigrationOnDowngrade()
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
