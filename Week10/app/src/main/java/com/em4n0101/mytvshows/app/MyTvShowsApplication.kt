dc23e4c7fab20670d7d99cc09cec07572488ab53
Emmanuel Cruz <em4n0101@gmail.com>
2020-08-03 17:31:50 -0500

    (Code) Model module

--- Week10/app/src/main/java/com/em4n0101/mytvshows/app/MyTvShowsApplication.kt
+++ Week10/app/src/main/java/com/em4n0101/mytvshows/app/MyTvShowsApplication.kt
@@ -2,11 +2,6 @@ package com.em4n0101.mytvshows.app
 
 import android.app.Application
 import android.content.Context
-import androidx.room.Room
-import com.em4n0101.mytvshows.model.database.ShowsDatabase
-import com.em4n0101.mytvshows.networking.RemoteApi
-import com.em4n0101.mytvshows.networking.buildApiServices
-import com.em4n0101.mytvshows.model.repositories.ShowsRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -14,28 +9,7 @@ import org.koin.core.context.startKoin
 class MyTvShowsApplication: Application() {
     companion object {
         private lateinit var instance: MyTvShowsApplication
-
         fun getAppContext(): Context = instance.applicationContext
-
-        private val apiService by lazy { buildApiServices() }
-
-        val remoteApi by lazy { RemoteApi(apiService) }
-
-        val database: ShowsDatabase by lazy {
-            val dataBaseName = "shows_database"
-            Room.databaseBuilder(
-                getAppContext(),
-                ShowsDatabase::class.java,
-                dataBaseName
-            ).fallbackToDestructiveMigrationOnDowngrade()
-                .fallbackToDestructiveMigration()
-                .build()
-        }
-
-        val showsRepository: ShowsRepository by lazy {
-            val dao = database.showsDatabaseDao
-            ShowsRepository(dao, remoteApi)
-        }
     }
 
     override fun onCreate() {
@@ -45,7 +19,7 @@ class MyTvShowsApplication: Application() {
         startKoin {
             androidLogger()
             androidContext(this@MyTvShowsApplication)
-            modules(listOf(networkModule))
+            modules(listOf(networkModule, modelModule))
         }
     }
 }
